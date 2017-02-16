3ac0b1a5865254162a7a2b31a1e043ae2aa69162
Giovanni Orlando <gorlandor@gmail.com>
2017-02-16 15:11:31 -0400

    Setup Firebase and Update SW

---
+++ firebase.config.js
@@ -0,0 +1,9 @@
+// Initialize Firebase
+const config = {
+    apiKey: "AIzaSyDWnz8KzRY3c8MDUyDg4AkyXWhVcVodSQo",
+    authDomain: "divs-portfaux.firebaseapp.com",
+    databaseURL: "https://divs-portfaux.firebaseio.com",
+    storageBucket: "divs-portfaux.appspot.com",
+    messagingSenderId: "888604659380"
+};
+firebase.initializeApp(config);
